09141271d3a555c9cd9a4ac6a9c8057e484d1eca
akulka-lab <74815124+akulka-lab@users.noreply.github.com>
2025-03-29 20:37:35 +0300

    fix simulation main

--- at_tutoring_skills/core/service/simulation/dependencies.py
+++ at_tutoring_skills/core/service/simulation/dependencies.py
@@ -1,2 +1,20 @@
+from typing import Protocol
+
+from pydantic import BaseModel
+
+from at_tutoring_skills.core.errors.models import CommonMistake
 
 
+class IMistakeService(Protocol):
+    def create_mistake(self, user_id: int, event: str, object_name: str): ...
+        # try:
+        #     self.repository.create_mistake(mistake, user_id)
+        # except Exception as e:
+        #     raise ValueError(f"Failed to create mistake: {e}") from e
+
+class ITaskService:
+    def __init__(self, repository: IMistakeService):
+        self.repository = repository
+
+    def complete_task(self, user_id: int, event: str, object_name: str):
+        self.repository.create_mistake(user_id, event, object_name)
