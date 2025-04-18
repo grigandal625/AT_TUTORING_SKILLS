f44ec6afce136a9468391b2d3f116988699987ff
rmk6 <milana.kras.01@gmail.com>
2025-04-18 17:11:03 +0300

    fix

--- at_tutoring_skills/core/KBskills.py
+++ at_tutoring_skills/core/KBskills.py
@@ -58,7 +58,7 @@ class ATTutoringKBSkills(ATComponent):
     async def handle_knowledge_base_created(self, event: str, data: dict, auth_token: str):
         user_id = await self.get_user_id_or_token(auth_token)
         user, _ = await self.task_service.create_user(user_id)
-        msg = await self.task_service.get_variant_tasks_description(user, scip_completed=True)
+        msg = await self.task_service.get_variant_tasks_description(user, skip_completed=True)
 
         return {"msg": msg, "hint": msg, "kb_id": data["result"]["knowledgeBase"]["id"]}
 
